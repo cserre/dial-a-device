49c948a827eb055a12dc1fe8fb210b7e0ef73bfd
Cominch <dominic@luetjohann.com>
2013-11-20 05:14:57 +0100

    update user details

--- db/migrate/20131120030322_add_details_to_users.rb
+++ db/migrate/20131120030322_add_details_to_users.rb
@@ -2,5 +2,6 @@ class AddDetailsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :firstname, :string
     add_column :users, :lastname, :string
+    add_column :users, :sign, :string
   end
 end
