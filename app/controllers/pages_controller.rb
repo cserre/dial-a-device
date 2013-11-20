922eb5aea7791ae8fe3a9baff2d9606d5ba51c04
Cominch <dominic@luetjohann.com>
2013-11-20 03:41:49 +0100

    create start pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+  def welcome
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+
+  def about
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 LsiRailsPrototype::Application.routes.draw do
+
+
+  match 'about' => 'pages#about'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +53,7 @@ LsiRailsPrototype::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'pages#welcome'
 
   # See how all your routes lay out with "rake routes"
 
