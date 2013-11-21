1069d1340c424471f0aca9080858b30bbf09ed6f
Cominch <dominic@luetjohann.com>
2013-11-21 19:03:43 +0100

    mailer

--- config/application.rb
+++ config/application.rb
@@ -63,5 +63,7 @@ module LsiRailsPrototype
 
     config.action_dispatch.x_sendfile_header = "X-Accel-Redirect"
 
+    config.action_mailer.default_url_options = { :host => 'lsi-rails-prototype.herokuapp.com'}
+
   end
 end
