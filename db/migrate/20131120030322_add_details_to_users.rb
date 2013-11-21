82e75ed682fbdcc3e80a5d2baa32de3a5da3c9e6
Cominch <dominic@luetjohann.com>
2013-11-21 14:45:11 +0100

    migration fix 1

--- db/migrate/20131120030322_add_details_to_users.rb
+++ db/migrate/20131120030322_add_details_to_users.rb
@@ -1,7 +1,11 @@
 class AddDetailsToUsers < ActiveRecord::Migration
-  def change
+  def up
     add_column :users, :firstname, :string
     add_column :users, :lastname, :string
     add_column :users, :sign, :string
+  end
+
+  def down
+    drop_table :users
   end
 end
