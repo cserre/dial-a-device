f270ea158ade14f3ff6130241f7b7c3060b25bfa
Cominch <dominic@luetjohann.com>
2013-12-14 21:55:43 +0100

    change environment definition

--- config/environments/localserver.rb
+++ config/environments/localserver.rb
@@ -1,3 +1,13 @@
+ActionMailer::Base.smtp_settings = {
+  :address  => 'localhost',
+  :port   => '587',
+  :authentication => :plain,
+  :user_name  => 'username',
+  :password => 'password',
+  :domain => 'localdomain'
+}
+ActionMailer::Base.delivery_method ||= :smtp
+
 CarrierWave.configure do |config|
 
   config.storage = :file
