5e48c0cd7d8140469d757229000776366d7bf884
Cominch <dominic@luetjohann.com>
2014-05-09 14:56:40 +0200

    fix

--- app/controllers/molecules_controller.rb
+++ app/controllers/molecules_controller.rb
@@ -581,7 +581,7 @@ class MoleculesController < ApplicationController
     end
 
     def set_project
-      if current_user. nil? then 
+      if current_user.nil? then 
         @project = Project.where(["title = ?", "chemotion"]).first
       else
         if params[:project_id].nil? || params[:project_id].empty? then
